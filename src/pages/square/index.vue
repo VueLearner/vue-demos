126d3dbbfbf46c3dafb678827a964d02fdb01238
frankwu <frank994@qq.com>
2018-03-08 20:10:26 +0800

    resize stylus

--- src/pages/square/index.vue
+++ src/pages/square/index.vue
@@ -52,77 +52,77 @@
 <style lang="stylus" scoped>
   @import '../../common/colors.styl'
   @import '../../common/values.styl'
-  #wrapper
-    .top
+#wrapper
+  .top
+    width 100%
+    height 90px
+    position absolute
+    background-color white
+    box-shadow 0px 5px 20px bgcor
+    z-index 99
+    .top-one
       width 100%
-      height 90px
-      position absolute
+      height 40%
       background-color white
-      box-shadow 0px 5px 20px bgcor
-      z-index 99
-      .top-one
-        width 100%
-        height 40%
-        background-color white
-        .voice-logo
-          width 20px
-          height 20px
-          position absolute 
-          right 10px
-          top 10px
-      .top-two
-        width 100%
-        height 60%
-        .choose
-          width 60px
-          height 100%
-          margin-left 10px
-          .text-title
-            width 100%
-            height 60%
-            text-align center
-            line-height 40px
-            .text
-              font-size fth3
-        .dot
-          display flex
-          flex-direction row
-          justify-content space-between
-          .dot-dom
-            flex 1
-            text-align center
-    .container
+      .voice-logo
+        width 20px
+        height 20px
+        position absolute 
+        right 10px
+        top 10px
+.top-two
+  width 100%
+  height 60%
+  .choose
+    width 60px
+    height 100%
+    margin-left 10px
+    .text-title
       width 100%
-      height 620px
-      overflow-y auto
-      z-index 1
-      .search
-        width 100%
-        height 40px
-        margin-top 90px
-        background-color whitesmoke
-        position absolute
-        .search-in
-          width 340px
-          height 26px
-          background-color white
-          position relative
-          margin 7px auto
-          border-radius 5px
-          .search-logo-container
-            float left
-            margin 3px 10px
-            .search-logo
-              width 20px
-              height 20px
-              .search-font
-                float left
-                line-height 26px
-                .search-font-p
-                  font-size ftmd
-                  color gary
-      .moment
-        width 100%
-        height 500px
-        background-color seashell
+      height 60%
+      text-align center
+      line-height 40px
+      .text
+        font-size fth3
+.dot
+  display flex
+  flex-direction row
+  justify-content space-between
+  .dot-dom
+    flex 1
+    text-align center       
+.container
+  width 100% 
+  height 620px
+  overflow-y auto
+  z-index 1
+  .search
+    width 100%
+    height 40px
+    margin-top 90px
+    background-color whitesmoke
+    position absolute
+    .search-in
+      width 340px
+      height 26px
+      background-color white
+      position relative
+      margin 7px auto
+      border-radius 5px
+      .search-logo-container
+        float left
+        margin 3px 10px
+.search-logo
+  width 20px
+  height 20px
+  .search-font
+    float left
+    line-height 26px
+    .search-font-p
+      font-size ftmd
+      color gary
+.moment
+  width 100%
+  height 500px
+  background-color seashell
 </style>
